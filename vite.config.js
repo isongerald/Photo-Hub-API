04b7d187f443b579b9f860b05dfa6b273fd70e31
isongerald <36533236+isongerald@users.noreply.github.com>
2025-12-03 02:37:23 +0800

    Updated Vite configuration and removed redundant config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   define: {
-    API_KEY: JSON.stringify(process.env.API_KEY),
+    API_KEY: JSON.stringify(import.meta.env.VITE_API_KEY),
   },
   root: "./",
   build: {
