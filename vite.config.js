1858796b3040b74d8202d704f0455caf0f560589
isongerald <36533236+isongerald@users.noreply.github.com>
2025-12-03 02:30:27 +0800

    Updated Vite configuration to inject API_KEY

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  define: {
+    API_KEY: JSON.stringify(process.env.API_KEY),
+  },
   root: "./",
   build: {
     outDir: "dist",
